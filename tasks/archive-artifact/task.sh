005dbb327d5390af2c0f4f92a5d4abf53aaf308a
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-06-12 07:49:10 -0700

    2

--- tasks/archive-artifact/task.sh
+++ tasks/archive-artifact/task.sh
@@ -28,7 +28,7 @@ echo "--- Archive Artifact ---"
 POM_FILE="pom.xml"
 
 # Checks version is ok with branchname
-checkversion="$(checkVersion $BRANCHNAME $POM_FILE)"
+checkversion="$(checkVersion $CURRENT_BRANCH $POM_FILE)"
 echo "CheckVersion result=${checkversion}"
 
 # Check tag exists
@@ -36,8 +36,8 @@ tagexists="$(tagExists ${POM_FILE})"
 echo "TagExists result=${tagexists}"
 
 # Calculate next release based on tags
-PATCH_LEVEL=$(expr `git tag | grep '${BRANCHNAME}.[0-9][0-9]*\$' | awk -F '.' '{ print $3 }' | sort -n | tail -n 1` + 1 || echo 0)
-NEXT_RELEASE=${BRANCHNAME}.${PATCH_LEVEL}
+PATCH_LEVEL=$(expr `git tag | grep '${CURRENT_BRANCH}.[0-9][0-9]*\$' | awk -F '.' '{ print $3 }' | sort -n | tail -n 1` + 1 || echo 0)
+NEXT_RELEASE=${CURRENT_BRANCH}.${PATCH_LEVEL}
 echo "Calculated next release: ${NEXT_RELEASE}"
 
 if [[ $checkversion = "true" ]]
@@ -48,23 +48,22 @@ then
 
        #   git add .
 
-        #  git config --global user.name "YOUR NAME"
-         # git config --global user.email "YOUR EMAIL ADDRESS"
-
-         # git commit -m "Changed some-file.txt"
-
       echo "WARN: The software is already tagged with this release"
       NEW_POM_VERSION="${NEXT_RELEASE}-SNAPSHOT"
       NEW_POM_FILE="${POM_FILE}.new"
       python ${ROOT_FOLDER}/${TOOLS_RESOURCE}/python/modify-version-pom.py ${POM_FILE} ${NEW_POM_FILE} ${NEW_POM_VERSION}
       mv $NEW_POM_FILE $POM_FILE
+
+      # git config --global user.name "${GIT_NAME}"
+      # git config --global user.email "${GIT_EMAIL}"
       
       echo "WARN: Patched pom version with value ${NEW_POM_VERSION}"
+      #git -c http.sslVerify=false -c http.sslKey=${HOME}/.gitprivatekey/privatekey commit -a -m "Changed pom version from ${POM_VERSION} to ${NEW_POM_VERSION}"
       #git commit -a -m "Changed pom version from ${POM_VERSION} to ${NEW_POM_VERSION}"
     fi
 
     # Maven release prepare
-    #mvn -s ${MAVEN_SETTINGS_FILE} --batch-mode release:clean release:prepare -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE} -Drelease.arguments="-Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE}"
+    #mvn --batch-mode release:clean release:prepare -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE} -Drelease.arguments="-Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE}"
     echo "maven prepare"
 
     # Maven release perform
@@ -72,7 +71,7 @@ then
     echo "maven release"
 else
     POM_VERSION="$(getPomVersion $POM_FILE)"
-    echo "ERROR: Pom Version ${POM_VERSION} does not match release name ${BRANCHNAME}"
+    echo "ERROR: Pom Version ${POM_VERSION} does not match release name ${CURRENT_BRANCH}"
     exit 1
 fi
 
