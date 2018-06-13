f1205cfdadd6ef60ac954cceae91e7a84fc79aca
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-06-13 13:25:17 -0700

    2

--- tasks/app-register/task.sh
+++ tasks/app-register/task.sh
@@ -12,7 +12,6 @@ export REPO_RESOURCE=repo
 export TOOLS_RESOURCE=tools
 export OUTPUT_RESOURCE=out
 export KEYVALOUTPUT_RESOURCE=keyvalout
-export TRUSTSTORE_FILE="${ROOT_FOLDER}/${TOOLS_RESOURCE}/settings/${TRUSTSTORE}"
 
 # Source all usefull scripts
 source "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/tasks/source-all.sh
@@ -44,7 +43,7 @@ echo "-- Executing shell script ..."
 #will replace the environment variables in your file with their corresponding value. The variable names must consist solely of alphanumeric #or underscore ASCII characters, not start with a digit and be nonempty; otherwise such a variable reference is ignored.
 envsubst < ${ROOT_FOLDER}/${REPO_RESOURCE}/pcf-scdf-deploy-${TIM_ENVIRONMENT}/appRegister.df >> ${TMPDIR}/appRegister.df
 
-echo ${PASSED_SCDF_SERVER_URL}
+echo "PASSED_SCDF_SERVER_URL:${PASSED_SCDF_SERVER_URL}"
 
 java -jar ${ROOT_FOLDER}/${TOOLS_RESOURCE}/scdf/spring-cloud-dataflow-shell-1.5.1.RELEASE.jar --dataflow.uri="${PASSED_SCDF_SERVER_URL}"  --spring.shell.commandFile="${TMPDIR}/appRegister.df"
 
