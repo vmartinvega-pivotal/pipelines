4ece0ac84df45bcb4cba5c697b6288fbb6276562
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-06-14 02:38:52 -0700

    2

--- set_all_pipelines-local.sh
+++ set_all_pipelines-local.sh
@@ -1,3 +1,4 @@
 ./set_pipeline.sh pipeline-build credentials-build-local.yml pipeline-build.yml
 ./set_pipeline.sh pipeline-task-artifact-test credentials-build-local.yml pipeline-task-artifact-test.yml
 ./set_pipeline.sh pipeline-deploy-system-test credentials-deploy-local.yml pipeline-deploy.yml
+./set_pipeline.sh pipeline-deploy-notify credentials-deploy-local.yml pipeline-deploy-notify.yml
