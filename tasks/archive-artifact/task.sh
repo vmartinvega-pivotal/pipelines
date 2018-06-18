8b30998070e73b7f74dea3522791aa27801702b9
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-06-18 07:35:34 -0700

    2

--- tasks/archive-artifact/task.sh
+++ tasks/archive-artifact/task.sh
@@ -13,7 +13,6 @@ export TOOLS_RESOURCE=tools
 export OUTPUT_RESOURCE=out
 export KEYVALOUTPUT_RESOURCE=keyvalout
 export KEYVAL_RESOURCE=keyval
-export TRUSTSTORE_FILE="${ROOT_FOLDER}/${TOOLS_RESOURCE}/truststore/${TRUSTSTORE}"
 
 # Source all usefull scripts
 source "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/tasks/source-all.sh
@@ -37,6 +36,10 @@ echo "TagExists result=${tagexists}"
 
 if [[ $checkversion = "true" ]]
 then
+    cp ${ROOT_FOLDER}/${TOOLS_RESOURCE}/truststore/${TRUSTSTORE} .
+ 
+    git config --global http.sslKey "${HOME}/.gitprivatekey/privatekey"
+    git config --global http.sslVerify false
     git config --global user.name "${GIT_NAME}"
     git config --global user.email "${GIT_EMAIL}"
     
@@ -54,13 +57,14 @@ then
       mv $NEW_POM_FILE $POM_FILE
       
       echo "WARN: Patched pom version with value ${NEW_POM_VERSION}"
-      git -c http.sslVerify=false -c http.sslKey=${HOME}/.gitprivatekey/privatekey commit -a -m "[ci skip] Changed pom version from ${POM_VERSION} to ${NEW_POM_VERSION}"
-      #git commit -a -m "[ci skip] Changed pom version from ${POM_VERSION} to ${NEW_POM_VERSION}"
+      git commit -a -m "[ci skip] Changed pom version from ${POM_VERSION} to ${NEW_POM_VERSION}"
     fi
 
     git checkout -f ${CURRENT_BRANCH}
 
-    mvn --batch-mode release:clean release:prepare release:perform -Drelease.arguments="-Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE} -Dsonar.branch=${SONAR_BRANCH}" -Dresume=false -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Dmaven.test.skip=true -DskipITs -Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE} -DscmCommentPrefix="[ci skip]" ${BUILD_OPTIONS}
+    mvn --batch-mode release:clean release:prepare release:perform -Drelease.arguments="-Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Djavax.net.ssl.trustStore=${TRUSTSTORE} -Dsonar.branch=${SONAR_BRANCH}" -Dresume=false -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Djavax.net.ssl.trustStore=${TRUSTSTORE} -DscmCommentPrefix="[ci skip]" -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
+
+    #mvn --batch-mode release:clean release:prepare release:perform -Drelease.arguments="-Djavax.net.ssl.trustStore=${TRUSTSTORE} -Dsonar.branch=${SONAR_BRANCH}" -Dresume=false -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Dmaven.test.skip=true -DskipITs -Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE} -DscmCommentPrefix="[ci skip]" ${BUILD_OPTIONS}
     
     # Maven release perform
     #mvn --batch-mode release:perform -Drelease.arguments="-Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE} -Dsonar.branch=${SONAR_BRANCH}" -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Djavax.net.ssl.trustStore=${TRUSTSTORE_FILE}"
