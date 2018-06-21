02256d353fccbf1a69fcb46942b3960b9fa9083d
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-06-21 01:43:16 -0700

    2

--- tasks/deploy/settingup-environment/task.sh
+++ tasks/deploy/settingup-environment/task.sh
@@ -22,7 +22,10 @@ exportKeyValProperties
 
 cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
 
-echo "-- Setting up scdf server ..."
+# TODO: Can be the latest version in the git repository or configured!!
+export PASSED_TAG_VERSION_DEPLOYING="V1.0.2"
+
+echo "-- Setting up Environment for version ${PASSED_TAG_VERSION_DEPLOYING} and environment to deploy ${ENVIRONMENT_DEPLOYING}..."
 
 # Login PCF
 cfLogin ${PWS_API} ${PWS_USER} ${PWS_PWD} ${PWS_ORG} ${PWS_SPACE}
@@ -30,16 +33,13 @@ cfLogin ${PWS_API} ${PWS_USER} ${PWS_PWD} ${PWS_ORG} ${PWS_SPACE}
 # Get the urls for the PCF and stores them in environment variables
 getPCFUrls ${PWS_ORG} ${PWS_SPACE}
 
-# Deploys scdf server with a specific service name and service plan in the configured organization and space
 
+# Checks if is needed to deploy scdf server
 if [[ ${DEPLOY_SCDF_SERVICE_INSTANCE} = "true" ]]
 then
   echo "DEBUG: Deploying SCDF ..."
 
-  # TODO: Can be the latest version in the git repository or configured!!
-  export PASSED_TAG_VERSION_DEPLOYING="V1.0.2"
-
-  # Deploys the scdf server
+  # Deploys scdf server with a specific service name and service plan in the configured organization and space
   cfSCDFDeploy ${SCDF_SERVICE_NAME} ${SCDF_SERVICE_PLAN} ${ENVIRONMENT_DEPLOYING} ${TAG_VERSION_DEPLOYING}
 
   # Modifies the environment configuration for the scdf server created to have the right nexus url, user and password
@@ -52,7 +52,17 @@ else
   export PASSED_SCDF_SERVER_URL=${SCDF_SERVER_URL}
 fi
 
-echo "-- Setting up scdf server ..."
+
+# Checks if is needed to rabbitmq instance
+if [[ ${DEPLOY_RABBITMQ_SERVICE_INSTANCE} = "true" ]]
+then
+  echo "DEBUG: Deploying RabbitMQ ..."
+  
+  # Deploying rabbitmq instance
+  pcfSetupRabbitService ${RABBITMQ_SERVICE_NAME} ${RABBITMQ_SERVICE_PLAN} ${ENVIRONMENT_DEPLOYING} ${TAG_VERSION_DEPLOYING}
+fi
+
+echo "-- Setting up Environment for version ${PASSED_TAG_VERSION_DEPLOYING} and environment to deploy ${ENVIRONMENT_DEPLOYING}..."
 
 # Adding values to keyvalout
 passKeyValProperties
