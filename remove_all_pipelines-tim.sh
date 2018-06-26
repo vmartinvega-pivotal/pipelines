dba31e3bdf06309ab840a4f79e68a41f835b7849
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-06-26 10:53:16 -0700

    2

--- remove_all_pipelines-tim.sh
+++ remove_all_pipelines-tim.sh
@@ -1,4 +1,4 @@
-#./remove_pipeline-tim.sh pipeline-build
+./remove_pipeline-tim.sh build-snapshoot-sdp-demo-clienti
 ./remove_pipeline-tim.sh build-release-sdp-demo-clienti
 #./remove_pipeline-tim.sh deploy-systemtest
 #./remove_pipeline-tim.sh pipeline-deploy-notify
--- set_all_pipelines-tim.sh
+++ set_all_pipelines-tim.sh
@@ -1,4 +1,5 @@
-./set_pipeline-tim.sh  build-release-sdp-demo-clienti credentials-build-tim.yml pipeline-build.yml
+#./set_pipeline-tim.sh  build-release-sdp-demo-clienti credentials-build-tim.yml pipeline-build.yml
+./set_pipeline-tim.sh  build-snapshoot-sdp-demo-clienti credentials-snapshoot-tim.yml pipeline-snapshoot.yml
 #./set_pipeline.sh  pipeline-build-sdp-demo-clienti credentials-build-tim.yml pipeline-build.yml
 #./set_pipeline-tim.sh  pipeline-task-artifact-test-sdp-demo-clienti credentials-build-tim.yml pipeline-task-artifact-test.yml
 #./set_pipeline.sh  pipeline-deploy-system-test credentials-deploy-tim.yml pipeline-deploy.yml
