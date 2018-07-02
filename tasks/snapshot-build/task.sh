0a1302bd1461cb4df34589c66fed721ad8ae5617
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-02 05:00:53 -0700

    2

--- tasks/snapshot-build/task.sh
+++ tasks/snapshot-build/task.sh
@@ -26,8 +26,10 @@ export TRUST_STORE_FILE=${ROOT_FOLDER}/${TOOLS_RESOURCE}/truststore/${TRUSTSTORE
 
 echo "--- Snapshoot Artifact ---"
 
+echo "El nuevo"
+
 #mvn deploy -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE}
-mvn deploy -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE}
+mvn -s /root/.m2/settings.xml deploy -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE}
 
 echo "--- Snapshoot Artifact ---"
 echo ""
