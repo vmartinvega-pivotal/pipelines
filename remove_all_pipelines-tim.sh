f4fd5923246f71542e2cc328c9ca4b4020ed7608
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-04 04:14:06 -0700

    2

--- remove_all_pipelines-tim.sh
+++ remove_all_pipelines-tim.sh
@@ -1,5 +1,6 @@
 #./remove_pipeline-tim.sh snapshot-dispositiva-processor
 ./remove_pipeline-tim.sh release-dispositiva-processor
+./remove_pipeline-tim.sh release-logical-microservice
 #./remove_pipeline-tim.sh deploy-systemtest-logicalmicroservice
 #./remove_pipeline-tim.sh build-snapshoot-logical
 #./remove_pipeline-tim.sh deploy-systemtest
