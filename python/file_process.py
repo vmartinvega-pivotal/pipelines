fa27b9760184809d42a3ef91b9e747aec6ca093d
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-04 01:19:27 -0700

    2

--- python/file_process.py
+++ python/file_process.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
   try:
       #Check argument number
       if len(sys.argv) > 5 or len(sys.argv) < 5:
-          print("Invalid argument number. Usage: file_process.py list_file(path) template_file(path) output_file(path) json_output_file(path)")
+          print("Invalid argument number. Usage: file_process.py list_file(path) template_file(path) output_file(path) properties_output_file(path)")
           sys.exit()
 
       listfile = str(sys.argv[1])
       templatefile = str(sys.argv[2])
       resultfile = str(sys.argv[3])
-      jsonfile = str(sys.argv[4])
+      propertiesfile = str(sys.argv[4])
 
       # Check parameters
       if not os.path.isfile(listfile):
@@ -31,20 +31,20 @@ if __name__ == "__main__":
          print("Output file already exist. Exiting...".format(resultfile))
          sys.exit()
 
-      if os.path.isfile(jsonfile):
-         print("JSON file already exist. Exiting...".format(jsonfile))
+      if os.path.isfile(propertiesfile):
+         print("Properties file already exist. Exiting...".format(propertiesfile))
          sys.exit()
-
-      print("Output file is: " + resultfile)
 
       # Open and get through it line by line
       with open(templatefile) as tf:
            for line in tf:
                # Search and match valid Regex line
                line_tf_match = re.search('(.+?)://(.+?):#VERSION#', line)
+               line_tf_match = re.search('(.+?)\.(.+?)=maven://(.+?):#VERSION#', line)
                if line_tf_match:
-                    template_line = line_tf_match.group(2)
-                    init_line = line_tf_match.group(1)
+                    template_line = line_tf_match.group(3)
+                    init_line = line_tf_match.group(1) + "." + line_tf_match.group(2) + "=maven"
+                    app_name = line_tf_match.group(2).upper().replace('-', '_') 
                     print("Template line found: " + template_line)
 
                     founded_version = False
@@ -68,11 +68,15 @@ if __name__ == "__main__":
                                       with open(resultfile, 'a') as outputfile:
                                           outputfile.write(init_line + "://" + output + '\n')
                                           founded_version = True
+                                      with open(propertiesfile, 'a') as outputfile:
+                                          outputfile.write(app_name + "=" + version + '\n')
+
                          # If not found the version for this line, write to the output file with special tag #VERSION#
                          if not founded_version:
                              print("Version for this line not found: " + template_line)
                              with open(resultfile, 'a') as outputfile:
                                  outputfile.write(init_line + "://" + template_line + ":#VERSION#" + '\n')
+
                          # Close files
                          if not lf.closed:
                              lf.close()
@@ -80,36 +84,12 @@ if __name__ == "__main__":
                    with open(resultfile, 'a') as outputfile:
                        outputfile.write( line)
 
-      # Get JSON file from the output file
-      with open(resultfile) as output_plain:
-           groups, artifacts, versions = [], [], []
-           for line in output_plain:
-               line_output_match = re.search('(.+?):(.+?):(.+)', line)
-               if line_output_match:
-                    artifact = line_output_match.group(1)
-                    group = line_output_match.group(2)
-                    version = line_output_match.group(3)
-
-                    groups.append(group);
-                    artifacts.append(artifact);
-                    versions.append(version);
-
-
-
-           json_data = [{"group": g, "artifact": a, "version": v} for g, a, v in zip(groups, artifacts, versions)]
-
-           with open(jsonfile, 'w') as outjsonfile:
-               json.dump(json_data, outjsonfile)
-
       # Close files
       if not tf.closed:
         tf.close()
 
-      if not output_plain.closed:
-        output_plain.close()
-
   except Exception as e:
-      print("Unexpected error:" + e)
+      print( e)
 
 
 
