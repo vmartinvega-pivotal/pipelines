3fbb54c0fd7f967a90ef0c8f6b55f230febb2bbc
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-05 00:40:38 -0700

    2

---
+++ tasks/deploy/do-nothing/task.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e +x
+
+set -o errexit
+set -o errtrace
+set -o pipefail
+
+export ROOT_FOLDER
+ROOT_FOLDER="$( pwd )"
+export REPO_RESOURCE=repo
+export TOOLS_RESOURCE=tools
+export OUTPUT_RESOURCE=out
+export KEYVALOUTPUT_RESOURCE=keyvalout
+export KEYVAL_RESOURCE=keyval
+
+export TRUST_STORE_FILE=${ROOT_FOLDER}/${TOOLS_RESOURCE}/truststore/${TRUSTSTORE}
+
+# Source all usefull scripts
+source "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/tasks/source-all.sh
+
+# Add properties as environment variables
+exportKeyValProperties
+
+cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
+
+echo "--- Do Nothing ---"
+
+
+echo "--- Do Nothing ---"
+echo ""
+
+# Adding values to the next job
+passKeyValProperties
+
+cp -r "${ROOT_FOLDER}/${REPO_RESOURCE}"/. "${ROOT_FOLDER}/${OUTPUT_RESOURCE}/"
+
+echo "Done!!"
