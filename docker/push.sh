2ff2c1825f93462ee3d74e8913f5bfad7e1c0df7
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-11 01:38:42 -0700

    2

--- docker/push.sh
+++ docker/push.sh
@@ -1,4 +1,4 @@
 export DOCKER_ID_USER="vmartinvega"
 docker login
-docker tag cf-tools-tim ${DOCKER_ID_USER}/concourse-tools-tim:latest
+docker tag concourse-tools-tim ${DOCKER_ID_USER}/concourse-tools-tim:latest
 docker push ${DOCKER_ID_USER}/concourse-tools-tim
