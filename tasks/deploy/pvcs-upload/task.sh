642736f7633697de54b6667a1b66009893575ba1
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-12 23:05:55 -0700

    2

--- tasks/deploy/pvcs-upload/task.sh
+++ tasks/deploy/pvcs-upload/task.sh
@@ -30,6 +30,7 @@ echo "--- Pvcs Upload ---"
 # If a new release was created
 if [[ ${PASSED_NEW_LOGICAL_RELEASE} = "true" ]]
 then
+  echo "store-plaintext-passwords = no" >> /root/.subversion/servers
   # Get all binaries from file to be uploaded to PVCS
   echo "checkout pvcs url: ${PVCS_URL}"
   PVCS_PATH=${TMPDIR}/pvcs/vicente_test
