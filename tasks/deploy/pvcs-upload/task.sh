08d8a2274cf73b60f95d8b4b83a8932c7c288229
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-12 23:48:57 -0700

    2

--- tasks/deploy/pvcs-upload/task.sh
+++ tasks/deploy/pvcs-upload/task.sh
@@ -44,13 +44,15 @@ then
   mkdir ${FOLDER_TO_WORK}
   cd ${FOLDER_TO_WORK}
   
-  
+  # Add logical scripts
   if [ -f ${FOLDER_TO_WORK}/logical ]; then
     rm -Rf ${FOLDER_TO_WORK}/logical
   fi
   mkdir ${FOLDER_TO_WORK}/logical
 
   cp -R ${ROOT_FOLDER}/${REPO_RESOURCE} ${FOLDER_TO_WORK}/logical
+
+  svn add --force ${FOLDER_TO_WORK}
 
   #mkdir ${PVCS_PATH}/binaries
 
@@ -59,7 +61,7 @@ then
    #mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=${artifact} -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE} -Ddest=${PVCS_PATH}/binaries -  Dtransitive=false
   #done < "${TMPDIR}/${REPO_RESOURCE}/maven-binaries-file"
 
-  #svn commit -m "Logical microservice version ${PASSED_TAG_RELEASED_CREATED}"
+  svn commit -m "Logical microservice version ${PASSED_TAG_RELEASED_CREATED}"
 fi
 
 echo "--- Pvcs Upload ---"
