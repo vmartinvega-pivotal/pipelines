94b7cf010f14a739f314bed84b0690c35b705eb3
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-17 01:51:22 -0700

    2

--- tasks/testlogical/task.sh
+++ tasks/testlogical/task.sh
@@ -16,6 +16,8 @@ export KEYVAL_RESOURCE=keyval
 
 # Source all usefull scripts
 source "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/tasks/source-all.sh
+export TRUST_STORE_FILE=${ROOT_FOLDER}/${TOOLS_RESOURCE}/truststore/${TRUSTSTORE}
+chmod 777 ${TRUST_STORE_FILE}
 
 # Add properties as environment variables
 exportKeyValProperties
@@ -23,6 +25,22 @@ exportKeyValProperties
 cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
 
 echo "--- Test Logical ---"
+
+# Copy all contents for the repo to a new location
+cp -r ${ROOT_FOLDER}/${REPO_RESOURCE} ${TMPDIR}
+
+# Change location
+cd ${TMPDIR}/${REPO_RESOURCE}
+
+# Resolve ranges for the dependencies
+mvn versions:resolve-ranges -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE}
+
+# Get the dependencies for the logical microservice
+mvn dependency:list -DexcludeTransitive=true -DoutputFile=dependencies.list -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE}
+
+python ${ROOT_FOLDER}/${TOOLS_RESOURCE}/python/file_process.py dependencies.list app-descriptor-template.df apps-version-template.env apps-version.env maven-binaries-file
+
+cat apps-version.env
 
 echo "--- Test Logical ---"
 echo ""
