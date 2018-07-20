b7c2a23fc9aaacb364d2dcb25fac9b5a888f31cf
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-20 13:19:11 -0700

    2

--- tasks/logical/compile-logical/task.sh
+++ tasks/logical/compile-logical/task.sh
@@ -18,28 +18,19 @@ export KEYVAL_RESOURCE=keyval
 export TRUST_STORE_FILE=${ROOT_FOLDER}/${TOOLS_RESOURCE}/truststore/${TRUSTSTORE}
 chmod 777 ${TRUST_STORE_FILE}
 
-if [[ ${PASSED_NEW_LOGICAL_RELEASE} = "true" ]]
-then
-echo "TODO:"
-fi
-
 # Source all usefull scripts
 source "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/tasks/source-all.sh
 
 # Add properties as environment variables
 exportKeyValProperties
 
-echo "--- Deploy Streams ---"
+echo "--- Compile Logical ---"
 
 cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
 
 prepareScriptsToDeploy
 
-# DEPLOY STREAMS
-
-# DEPLOY STREAMS
-
-echo "--- Deploy Streams ---"
+echo "--- Compile Logical ---"
 echo ""
 
 # Adding values to the next job
