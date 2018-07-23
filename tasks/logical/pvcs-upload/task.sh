0bc019331a45bc4b5638364927f7b362d7d4cd3d
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-23 03:21:00 -0700

    2

--- tasks/logical/pvcs-upload/task.sh
+++ tasks/logical/pvcs-upload/task.sh
@@ -14,6 +14,9 @@ export OUTPUT_RESOURCE=out
 export KEYVALOUTPUT_RESOURCE=keyvalout
 export KEYVAL_RESOURCE=keyval
 
+export TRUST_STORE_FILE=${ROOT_FOLDER}/${TOOLS_RESOURCE}/truststore/${TRUSTSTORE}
+chmod 777 ${TRUST_STORE_FILE}
+
 # Source all usefull scripts
 source "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/tasks/source-all.sh
 
@@ -42,18 +45,30 @@ fi
 mkdir ${FOLDER_TO_WORK_IN_PVCS}
 cd ${FOLDER_TO_WORK_IN_PVCS}
 
-#TODO: COPIAR LO QUE CORRESPONDA, FUENTES  
-
 cp "${ROOT_FOLDER}/${REPO_RESOURCE}"/pom.xml ${FOLDER_TO_WORK_IN_PVCS} 
 
+# Get all sources
+# Get all physical microservices pom.xml files and jar files
+mvn -f pom.xml dependency:copy-dependencies -DexcludeTransitive=true -Dmdep.copyPom=true -DoutputDirectory=target/poms -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE}
+
+mkdir ${FOLDER_TO_WORK_IN_PVCS}/micros-sources
+for FILE in `ls target/poms/*.pom` 
+do
+  ARTIFACTID=$(getArtifactId ${FILE})
+  
+  #mvn -f ${FILE}  -DcheckoutDirectory=target/micros-sources/${ARTIFACTID} -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE}
+done
+cp -r target/micros-sources ${FOLDER_TO_WORK_IN_PVCS}/micros-sources
+
+cp -r compiled/ ${FOLDER_TO_WORK_IN_PVCS}
+mv ${FOLDER_TO_WORK_IN_PVCS}/compiled ${FOLDER_TO_WORK_IN_PVCS}/compiled-files
+cp app-descriptor.df ${FOLDER_TO_WORK_IN_PVCS}/compiled-files
+cp apps-version.env ${FOLDER_TO_WORK_IN_PVCS}/compiled-files
+
+mkdir ${FOLDER_TO_WORK_IN_PVCS}/micros-binaries
+cp target/poms/*.jar ${FOLDER_TO_WORK_IN_PVCS}/micros-binaries
+
 svn add --force ${FOLDER_TO_WORK_IN_PVCS}
-
-#mkdir ${PVCS_PATH}/binaries
-
-#while IFS= read -r artifact
-#do
-#mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=${artifact} -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE} -Ddest=${PVCS_PATH}/binaries -  Dtransitive=false
-#done < "${TMPDIR}/${REPO_RESOURCE}/maven-binaries-file"
 
 svn commit -m "Logical microservice version ${PASSED_TAG_RELEASED_CREATED}" --username=${PVCS_USERNAME} --password=${PVCS_PASSWORD}
 
