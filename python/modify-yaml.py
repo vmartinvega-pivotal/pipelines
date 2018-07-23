592cb5bd3ca22b0ec4767bb5c49981b8c529a62b
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-23 09:01:04 -0700

    2

--- python/modify-yaml.py
+++ python/modify-yaml.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
   file_name = str(sys.argv[1])
   new_branch = str(sys.argv[2])
   repo_names = str(sys.argv[3]).split(',')
+  output_file = str(sys.argv[4])
 
   config, ind, bsi = load_yaml_guess_indent(open(file_name))
   resources = config['resources']
@@ -17,4 +18,4 @@ if __name__ == "__main__":
       if resource['name'] in repo_names:
         resource['source']['branch'] = new_branch
 
-  ruamel.yaml.round_trip_dump(config, open('output.yml', 'w'), indent=ind, block_seq_indent=bsi)
+  ruamel.yaml.round_trip_dump(config, open(output_file, 'w'), indent=ind, block_seq_indent=bsi)
