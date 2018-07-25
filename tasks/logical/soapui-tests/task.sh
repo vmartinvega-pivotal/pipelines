2478a275ed0e78fa1913fda120236a76094dc273
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-25 05:46:17 -0700

    2

--- tasks/logical/soapui-tests/task.sh
+++ tasks/logical/soapui-tests/task.sh
@@ -26,7 +26,8 @@ cd "${ROOT_FOLDER}/${TESTS_RESOURCE}" || exit
 
 echo "-- Running SaopUI tests"
 
-nohup /opt/SoapUI/bin/testrunner.sh -s"consistenze TestSuite" -r -a -j -J -GAmbiente=COLLEVO -f./Reports ./ID_20_Consistenze-soapui-project.xml &
+#nohup /opt/SoapUI/bin/testrunner.sh -s"consistenze TestSuite" -r -a -j -J -GAmbiente=COLLEVO -f./Reports ./ID_20_Consistenze-soapui-project.xml &
+/opt/SoapUI/bin/testrunner.sh -s"consistenze TestSuite" -r -a -j -J -GAmbiente=COLLEVO -f./Reports ./ID_20_Consistenze-soapui-project.xml
 PROC_ID=$!
 
 while kill -0 "$PROC_ID" >/dev/null 2>&1; do
