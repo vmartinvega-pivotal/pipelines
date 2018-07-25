3c55521932cfa733a1bb980669b63a0daec2fd2c
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-07-25 05:33:07 -0700

    Pipelines working with job changing branches

--- tasks/logical/soapui-tests/task.sh
+++ tasks/logical/soapui-tests/task.sh
@@ -12,6 +12,7 @@ export REPO_RESOURCE=repo
 export TOOLS_RESOURCE=tools
 export TESTS_RESOURCE=tests
 export OUTPUT_RESOURCE=out
+export OUTPUT_TESTS=outtests
 export KEYVALOUTPUT_RESOURCE=keyvalout
 export KEYVAL_RESOURCE=keyval
 
@@ -25,18 +26,27 @@ cd "${ROOT_FOLDER}/${TESTS_RESOURCE}" || exit
 
 echo "-- Running SaopUI tests"
 
-cd Projects/consistenze
+cd 
 
 nohup /opt/SoapUI/bin/testrunner.sh -s"consistenze TestSuite" -r -a -j -J -GAmbiente=COLLEVO -f./Reports ./ID_20_Consistenze-soapui-project.xml &
 PROC_ID=$!
 
 while kill -0 "$PROC_ID" >/dev/null 2>&1; do
-    echo "PROCESS IS RUNNING"
+    echo "Tests running ..."
     sleep 5
 done
-echo "PROCESS TERMINATED"
+echo "Tests finished!!"
 
-ls ./Reports
+cp -r "${ROOT_FOLDER}/${TESTS_RESOURCE}"/Projects/consistenze/. "${ROOT_FOLDER}/${OUTPUT_TESTS}/"
+
+echo RESULT=$(ls ./Reports/ | grep FAILED | wc -l)
+if [[ $RESULT = "0" ]]
+then
+  echo "Success!!"
+else
+  echo "Failure!!"
+  exit 1
+fi
 
 echo "-- Running SaopUI tests"
 
