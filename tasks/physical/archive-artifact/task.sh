f35a259e0b49e9b0bb7284594f6bab48a0e10de6
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-08-20 08:44:30 -0700

    Generic commit

--- tasks/physical/archive-artifact/task.sh
+++ tasks/physical/archive-artifact/task.sh
@@ -65,7 +65,6 @@ then
 
     #TODO: SI Branch release (number.number) releaese se ejecuta maven release, en otro caso se ejecuta maven deploy
     #mvn --batch-mode release:clean release:prepare release:perform -Drelease.arguments="-Dmaven.javadoc.skip=true -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE} -Dsonar.branch=${SONAR_BRANCH}" -Dresume=false -Dusername=${USERNAME} -Dpassword=${PASSWORD} -Djavax.net.ssl.trustStore=${TRUST_STORE_FILE} -DscmCommentPrefix="[ci skip]" -Dmaven.javadoc.skip=true
-    
 else
     POM_VERSION="$(getPomVersion $POM_FILE)"
     echo "ERROR: Pom Version ${POM_VERSION} does not match release name ${CURRENT_BRANCH}"
