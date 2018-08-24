1de0547efa16db7a90945d0d52422f6bebd60014
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2018-08-23 23:41:20 -0700

    Generic commit

---
+++ tasks/physical/integration-test/task.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e +x
+
+set -o errexit
+set -o errtrace
+set -o pipefail
+
+export ROOT_FOLDER
+ROOT_FOLDER="$( pwd )"
+export REPO_RESOURCE=repo
+export TOOLS_RESOURCE=tools
+export OUTPUT_RESOURCE=out
+export KEYVALOUTPUT_RESOURCE=keyvalout
+export KEYVAL_RESOURCE=keyval
+
+# Source all usefull scripts
+source "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/tasks/source-all.sh
+
+# Add properties as environment variables
+exportKeyValProperties
+
+cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
+
+echo "--- Integration Test ---"
+echo "Running Integration tests ..."
+echo "--- Integration Test ---"
+echo ""
+
+# Adding values to the next job
+passKeyValProperties
+
+cp -r "${ROOT_FOLDER}/${REPO_RESOURCE}"/. "${ROOT_FOLDER}/${OUTPUT_RESOURCE}/"
+
+echo "Done!!"
